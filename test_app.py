d2c1660ea2238f1e4f112b3f982ff2210b478330
golds19 <gsbensaint19@gmail.com>
2025-05-14 11:05:09 +0900

    solved issues in test_app.py

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ from app import app
 
 def test_home():
     """Test the home page"""
-    response = app.test_clien().get('/')
+    response = app.test_client().get('/')
 
     assert response.status_code == 200
     assert response.data == b"Hello world"
